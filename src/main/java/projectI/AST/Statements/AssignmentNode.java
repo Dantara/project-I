d8bac395d407f8adfb50b8b2c7955f6f240c0a19
Alwa0 <a.paukova@innopolis.university>
2020-09-11 22:38:33 +0300

    add comments to Statements and ProgramNode

--- src/main/java/projectI/AST/Statements/AssignmentNode.java
+++ src/main/java/projectI/AST/Statements/AssignmentNode.java
@@ -10,15 +10,29 @@ public class AssignmentNode implements StatementNode {
     public final ModifiablePrimaryNode modifiable;
     public final ExpressionNode assignedValue;
 
+    /**
+     * A constructor for initializing objects of class AssignmentNode
+     * @param modifiable is a modifiable primary
+     * @param assignedValue is an expression assigned to the modifiable primary
+     */
     public AssignmentNode(ModifiablePrimaryNode modifiable, ExpressionNode assignedValue) {
         this.modifiable = modifiable;
         this.assignedValue = assignedValue;
     }
 
+    /**
+     * Find a start position in the source code
+     * @return the position
+     */
     public CodePosition getStartPosition() {
         return modifiable.startPosition;
     }
 
+    /**
+     * Check whether this object is equal to the passed one.
+     * @param o the object to check the equality with
+     * @return true if this object is equal to the passed one, false otherwise.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -28,16 +42,28 @@ public class AssignmentNode implements StatementNode {
                 Objects.equals(assignedValue, that.assignedValue);
     }
 
+    /**
+     * Calculate the hashcode of the object.
+     * @return hashcode
+     */
     @Override
     public int hashCode() {
         return Objects.hash(modifiable, assignedValue);
     }
 
+    /**
+     * Get the formatted representation of the string.
+     * @return the object as a string
+     */
     @Override
     public String toString() {
         return modifiable + " := " + assignedValue;
     }
 
+    /**
+     * Check if node is valid
+     * @return true if this object is valid, false otherwise.
+     */
     @Override
     public boolean validate() {
         return modifiable != null && modifiable.validate() &&
